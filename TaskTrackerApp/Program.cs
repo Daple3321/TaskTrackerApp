deeb7e2ccb0961425b532f4935d3463ddfd06ee8
Egor <egorchik.kobylkin@gmail.com>
2024-11-07 00:20:13 +0300

    Добавьте файлы проекта.

---
+++ TaskTrackerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace TaskTrackerApp
+{
+    class Program
+    {
+        public static List<Task> tasks = new List<Task>();
+
+
+        static void Main(string[] args)
+        {
+            Task test = new Task(1, "Some task...");
+
+            tasks.Add(test);
+
+            foreach (var task in tasks)
+            {
+                task.Print();
+            }
+        }
+    }
+
+}
---
+++ TaskTrackerApp/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTrackerApp
+{
+    internal class Task
+    {
+        public string description = "No Description.";
+
+        public enum TaskState
+        {
+            Done,
+            InProgress,
+            InDesign,
+            None,
+        }
+        public TaskState State { get; set; }
+
+        public int id;
+        public TimeSpan createdAt;
+        public TimeSpan updatedAt;
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Task: " + description + "\n" +
+                "State: " + State.ToString() + "\n" +
+                "Id: " + id + "\n" +
+                "Created at: " + createdAt.Days + "\n" +
+                "Updated at: " + updatedAt.Days + "\n");
+        }
+
+        public Task(int assignedId, string desc = null)
+        { 
+            description = desc;
+            State = TaskState.InProgress;
+
+            id = assignedId;
+        }
+    }
+}
